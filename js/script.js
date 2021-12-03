4b3f48301aee2f96354935a2fe02135c027021f2
An0n1myS <72464221+An0n1myS@users.noreply.github.com>
2021-12-03 06:25:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function openMagicBall() {
+	window.open('https://an0n1mys.github.io/Magic-Ball/');
+}
+function openWarringKingdom() {
+	window.open('https://an0n1mys.github.io/Warring-Kingdom/');
+}
+function openSpinGame() {
+	window.open('https://an0n1mys.github.io/SpinGame/');
+}
+function openTwentyOne() {
+	window.open('https://an0n1mys.github.io/Twenty-one/');
+}
